f6b1fe93c7d34eccd66015c58d5d43fbae750e53
heatherc4 <70985599+heatherc4@users.noreply.github.com>
2020-10-20 10:02:38 +0800

    Initial commit

---
+++ Scripts/CubeMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour
+{
+
+    public float speed = 10f;
+    public Rigidbody rb;
+    public bool cubeIsOnTheGround = true;
+
+    // use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float vertical = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.Translate(horizontal, 0, vertical);
+
+        if (Input.GetButtonDown("Jump") && cubeIsOnTheGround)
+        {
+            rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+            cubeIsOnTheGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            cubeIsOnTheGround = true;
+    }
+}
